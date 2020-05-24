c85ced2deeac795f88e315790380df0faad1b7ca
idongwannabe <61856502+idongwannabe@users.noreply.github.com>
2020-05-24 09:56:52 +0800

    Add files via upload

---
+++ Carousel/js/tools.js
@@ -0,0 +1,23 @@
+function move(obj, attr, target, speed, callback){
+	clearInterval(obj.timer);
+	var current = parseInt(getStyle(obj, attr));
+	if(current > target){
+		speed = -speed;
+	}
+	obj.timer = setInterval(function(){
+		var oldValue = parseInt(getStyle(obj, attr));
+		var newValue = oldValue + speed;
+		if( (speed < 0 && newValue < target) || (speed > 0 && newValue > target) ){
+			newValue = target;
+		}
+		obj.style[attr] = newValue + "px";
+		if(newValue === target){
+			clearInterval(obj.timer);
+			callback();
+		}
+	}, 30);
+}
+			
+function getStyle(obj, name){
+	return getComputedStyle(obj, null)[name];
+}
